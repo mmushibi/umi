0bb6c57314bca269f0d4f75696c25a34f2a232f5
Sepio <sepiopixel@gmail.com>
2026-01-12 17:31:58 +0200

    Delete backend directory

--- backend/src/UmiHealth.Application/Services/IDataSyncService.cs
+++
@@ -1,5 +0,0 @@
-using System.Threading.Tasks;
-using UmiHealth.Application.DTOs;
-
-// This file is deprecated - IDataSyncService interface is now defined in DataSyncService.cs
-// to avoid conflicts and duplicate definitions.
--- backend/src/UmiHealth.Identity/Services/IPasswordService.cs
+++
@@ -1,9 +0,0 @@
-namespace UmiHealth.Identity.Services;
-
-public interface IPasswordService
-{
-    string HashPassword(string password);
-    bool VerifyPassword(string password, string hash);
-    bool IsValidPassword(string password);
-    string GenerateRandomPassword(int length = 12);
-}
